26d7ad2b5551713504f776fd0029c71a9e8622d0
aravind <aravind@baabte.com>
2019-11-20 15:28:05 +0530

    update settings file

--- heroku_project/settings.py
+++ heroku_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x8qke1+r%*bzn@&-ek&*2zwg(j=(b3pqzkvh@6o(t7w4!&i(z3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-herokuapp.herokuapp']
 
 
 # Application definition
